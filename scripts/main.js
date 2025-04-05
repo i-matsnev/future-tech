4b00c4f31d1a64d9b452f161be66fed5202edb21
HopeUsersGH <flash.matsnev132@gmail.com>
2025-04-05 23:59:32 +0700

    Finished blog page, created podcasts page and finished hero-alt section

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
+import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js"
 import Header from './Header.js'
 import TabsCollection from "./Tabs.js"
 import VideoPlayerCollection from "./VideoPlayer.js";
-import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js"
+import ExpandableContentCollection from "./ExpandableContent.js";
+
+defineScrollBarWidthCSSVar()
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
+new ExpandableContentCollection()
 
-defineScrollBarWidthCSSVar()
